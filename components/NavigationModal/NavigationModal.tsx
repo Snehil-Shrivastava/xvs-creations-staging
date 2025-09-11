cbfdf1d9b9825dd5306152e90d6a36f795c3c659
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2025-09-11 14:21:15 +0530

    navigation modal 1024-1280 responsiveness completed

--- components/NavigationModal/NavigationModal.tsx
+++ components/NavigationModal/NavigationModal.tsx
@@ -9,6 +9,8 @@ import Facebook from "@/SVGs/Facebook";
 import LinkedIn from "@/SVGs/LinkedIn";
 import Twitter from "@/SVGs/Twitter";
 
+import { MainNavLinks, NavLinksBody } from "@/data/NavigationModalData";
+
 const NavigationModal = ({
   isOpen,
   toggle,
@@ -16,9 +18,8 @@ const NavigationModal = ({
   isOpen: boolean;
   toggle: () => void;
 }) => {
-  // if (!isOpen) {
-  //   return null;
-  // }
+  const mainNavLinks = MainNavLinks;
+  const navLinksBody = NavLinksBody;
 
   return (
     <div
@@ -26,90 +27,30 @@ const NavigationModal = ({
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
-      <div className="absolute right-0 xl:max-1440p:w-[78%] flex flex-col xl:max-1440p:top-13 xl:max-1440p:gap-20">
-        <div className="flex gap-10 max-[426px]:gap-[22px] xl:max-2xl:gap-15 min-[1536px]:max-[1906px]:gap-30 min-[1906px]:gap-40 max-sm:text-[0.75rem] sm:max-lg:text-[1.2rem] lg:max-xl:text-[1.4rem] xl:max-2xl:text-[1.375rem] min-[1536px]:max-[1906px]:text-[1.8rem] min-[1906px]:text-[2rem] font-extralight">
-          <Link href="/about" onClick={toggle}>
-            About
-          </Link>
-          <Link href="/services" onClick={toggle}>
-            Work
-          </Link>
-          <Link href="/about" onClick={toggle}>
-            Blog
-          </Link>
-          <Link href="/contact" onClick={toggle}>
-            Contact
-          </Link>
+      <div className="absolute right-0 lg:max-xl:w-[75%] xl:max-1440p:w-[78%] flex flex-col lg:max-xl:top-[6dvh] xl:max-1440p:top-13 lg:max-xl:gap-[5dvh] xl:max-1440p:gap-[8dvh]">
+        <div className="flex lg:max-xl:gap-10 xl:max-1440p:gap-15 lg:max-xl:text-[1.4rem] xl:max-1440p:text-[1.375rem] font-extralight">
+          {mainNavLinks.map((link, index) => (
+            <Link key={index} href={link.url} onClick={toggle}>
+              {link.label}
+            </Link>
+          ))}
         </div>
         <div>
-          <div className="max-xl:flex max-xl:flex-col max-sm:gap-[3dvh] gap-9 grid grid-cols-2 xl:max-2xl:gap-15 min-[1536px]:max-[1906px]:gap-18 min-[1906px]:gap-25">
-            <div>
-              <h3 className="max-sm:text-[0.625rem] sm:max-lg:text-[0.875rem] lg:max-xl:text-[1rem] min-[1536px]:max-[1906px]:text-[1.2rem] min-[1906px]:text-[1.4rem]">
-                Idenitiy and Design
-              </h3>
-              <Link
-                href="/branding"
-                className={`font-calSans max-sm:text-[1.8rem]/[2.5rem] sm:max-lg:text-[2rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-2xl:text-[3rem]/[3.75rem] min-[1536px]:max-[1906px]:text-[3.2rem]/[4rem] min-[1906px]:text-[3.8rem]/[4.5rem]`}
-                onClick={toggle}
-              >
-                Branding
-              </Link>
-            </div>
-            <div>
-              <h3 className="max-sm:text-[0.625rem] sm:max-lg:text-[0.875rem] lg:max-xl:text-[1rem] min-[1536px]:max-[1906px]:text-[1.2rem] min-[1906px]:text-[1.4rem]">
-                Print and Digital
-              </h3>
-              <Link
-                href="/branding"
-                className={`font-calSans max-sm:text-[1.8rem]/[2.5rem] sm:max-lg:text-[2rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-2xl:text-[3rem]/[3.75rem] min-[1536px]:max-[1906px]:text-[3.2rem]/[4rem] min-[1906px]:text-[3.8rem]/[4.5rem]`}
-              >
-                Graphic Design
-              </Link>
-            </div>
-            <div>
-              <h3 className="max-sm:text-[0.625rem] sm:max-lg:text-[0.875rem] lg:max-xl:text-[1rem] min-[1536px]:max-[1906px]:text-[1.2rem] min-[1906px]:text-[1.4rem]">
-                Motion Craft
-              </h3>
-              <Link
-                href="/branding"
-                className={`font-calSans max-sm:text-[1.8rem]/[2.5rem] sm:max-lg:text-[2rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-2xl:text-[3rem]/[3.75rem] min-[1536px]:max-[1906px]:text-[3.2rem]/[4rem] min-[1906px]:text-[3.8rem]/[4.5rem]`}
-              >
-                Editing and Motion Graphics
-              </Link>
-            </div>
-            <div>
-              <h3 className="max-sm:text-[0.625rem] sm:max-lg:text-[0.875rem] lg:max-xl:text-[1rem] min-[1536px]:max-[1906px]:text-[1.2rem] min-[1906px]:text-[1.4rem]">
-                Interface and Experience
-              </h3>
-              <Link
-                href="/branding"
-                className={`font-calSans max-sm:text-[1.8rem]/[2.5rem] sm:max-lg:text-[2rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-2xl:text-[3rem]/[3.75rem] min-[1536px]:max-[1906px]:text-[3.2rem]/[4rem] min-[1906px]:text-[3.8rem]/[4.5rem]`}
-              >
-                UI/UX
-              </Link>
-            </div>
-            <div>
-              <h3 className="max-sm:text-[0.625rem] sm:max-lg:text-[0.875rem] lg:max-xl:text-[1rem] min-[1536px]:max-[1906px]:text-[1.2rem] min-[1906px]:text-[1.4rem]">
-                Design and Development
-              </h3>
-              <Link
-                href="/branding"
-                className={`font-calSans max-sm:text-[1.8rem]/[2.5rem] sm:max-lg:text-[2rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-2xl:text-[3rem]/[3.75rem] min-[1536px]:max-[1906px]:text-[3.2rem]/[4rem] min-[1906px]:text-[3.8rem]/[4.5rem]`}
-              >
-                Web and App
-              </Link>
-            </div>
-            <div>
-              <h3 className="max-sm:text-[0.625rem] sm:max-lg:text-[0.875rem] lg:max-xl:text-[1rem] min-[1536px]:max-[1906px]:text-[1.2rem] min-[1906px]:text-[1.4rem]">
-                Design and Modelling
-              </h3>
-              <Link
-                href="/branding"
-                className={`font-calSans max-sm:text-[1.8rem]/[2.5rem] sm:max-lg:text-[2rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-2xl:text-[3rem]/[3.75rem] min-[1536px]:max-[1906px]:text-[3.2rem]/[4rem] min-[1906px]:text-[3.8rem]/[4.5rem]`}
-              >
-                3D Product and Animations
-              </Link>
-            </div>
+          <div className="max-xl:flex max-xl:flex-col grid grid-cols-2 lg:max-xl:gap-[4dvh] xl:max-1440p:gap-[7dvh]">
+            {navLinksBody.map((link, index) => (
+              <div key={index}>
+                <h3 className="lg:max-xl:text-[1rem] xl:max-1440p:text-[1.2rem]">
+                  {link.subheading}
+                </h3>
+                <Link
+                  href={link.url}
+                  className={`font-calSans lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-1440p:text-[3rem]/[3.75rem]`}
+                  onClick={toggle}
+                >
+                  {link.heading}
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </div>
@@ -126,7 +67,10 @@ const NavigationModal = ({
         <Link href="https://www.facebook.com/xVSCreations" target="_blank">
           <Facebook />
         </Link>
-        <Link href="https://www.linkedin.com/company/xvs-creations" target="_blank">
+        <Link
+          href="https://www.linkedin.com/company/xvs-creations"
+          target="_blank"
+        >
           <LinkedIn />
         </Link>
         <Link href="https://x.com/xvscreations" target="_blank">
---
+++ data/NavigationModalData.js
@@ -0,0 +1,61 @@
+export const MainNavLinks = [
+    {
+        id: 1,
+        label: 'About',
+        url: '/about'
+    },
+    {
+        id: 2,
+        label: 'Work',
+        url: '/services'
+    },
+    {
+        id: 3,
+        label: 'Blog',
+        url: '/'
+    },
+    {
+        id: 4,
+        label: 'Contact',
+        url: '/contact'
+    },
+]
+
+export const NavLinksBody = [
+    {
+        id: 1,
+        heading: 'Branding',
+        subheading: 'Identity and Design',
+        url: '/'
+    },
+    {
+        id: 2,
+        heading: 'Graphic Design',
+        subheading: 'Print and Digital',
+        url: '/'
+    },
+    {
+        id: 3,
+        heading: 'Editing and Motion Graphics',
+        subheading: 'Motion Craft',
+        url: '/'
+    },
+    {
+        id: 4,
+        heading: 'UI/UX',
+        subheading: 'Interface and Experience',
+        url: '/'
+    },
+    {
+        id: 5,
+        heading: 'Web and App',
+        subheading: 'Design and Development',
+        url: '/'
+    },
+    {
+        id: 6,
+        heading: '3D Product and Animations',
+        subheading: 'Design and Modelling',
+        url: '/'
+    },
+]

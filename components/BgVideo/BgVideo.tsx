8d76b8a19218e887e3a67d20e8359257dc059b16
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2025-09-12 16:15:04 +0530

    services background video preload

--- components/BgVideo/BgVideo.tsx
+++ components/BgVideo/BgVideo.tsx
@@ -2,7 +2,7 @@ import './BgVideo.css'
 
 const BgVideo = ({ src }: { src?: string }) => {
   return (
-    <video className="-scale-x-100 relative" width="100%" height="100%" autoPlay loop muted>
+    <video className="-scale-x-100 relative" width="100%" height="100%" preload='auto' autoPlay loop muted>
       <source src={src ? src : '/videos/services.mp4'} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
